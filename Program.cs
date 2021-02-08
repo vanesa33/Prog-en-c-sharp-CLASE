51288bb8fde3cca6f8df8ec5e2075f7dd438071d
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:10:23 -0300

    dentro de la carpeta clase archivo .csproj y .cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace clase
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Ventana calculadora = new Ventana();
+
+            calculadora.cerrar();
+            calculadora.maximizar();
+
+            Console.ReadKey();
+
+        }
+
+               
+    }
+
+    class Ventana
+    {
+        private int ancho = 15, altura = 20;
+        private string color = "rojo";
+
+        //metodo
+        public void cerrar()
+        {
+            Console.WriteLine("la ventana se cerro");
+
+        }
+        public void minimizar()
+        {
+            Console.WriteLine("la ventana se minimizo");
+
+        }
+        public void maximizar()
+        {
+            Console.WriteLine(" la ventana se agrando");
+
+        }
+    }
+
+}
